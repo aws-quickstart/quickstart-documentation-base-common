47ff2a2422d70d68df0b73e0c29d2852d37abcd0
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-06-02 17:03:20 -0500

    Adding script to configure git env

---
+++ .utils/configure_git_env.sh
@@ -0,0 +1,8 @@
+git remote update
+git fetch
+git checkout --track origin/gh-pages
+rm -rf docs/*
+git restore -s master docs
+git rm -r docs/common -r
+git restore -s master templates
+git submodule add https://github.com/aws-quickstart/quickstart-documentation-base-common.git docs/common
