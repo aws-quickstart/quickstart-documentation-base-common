aa23b04b2849623c4b156e58e710982074134aa3
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-06-03 10:34:07 -0500

    Fail on error

--- .utils/build_docs.sh
+++ .utils/build_docs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-asciidoctor --backend=html5 -o index.html -a allow-uri-read --doctype=book -a toc2 docs/index.adoc
+asciidoctor --backend=html5 -o index.html -w --failure-level ERROR --doctype=book -a toc2 docs/index.adoc
