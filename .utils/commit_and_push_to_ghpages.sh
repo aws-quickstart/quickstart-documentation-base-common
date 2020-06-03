bffdecaa1dbe8b63570241112a9b021c6cfe3624
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-06-02 20:14:00 -0500

    force-push to gh-pages

--- .utils/commit_and_push_to_ghpages.sh
+++ .utils/commit_and_push_to_ghpages.sh
@@ -19,7 +19,7 @@ if [ $? -ne 0 ]; then
 fi
 
 git remote set-url origin ${repo_uri}
-git push origin HEAD:${target_branch}
+git push origin HEAD:${target_branch} --force
 
 #git remote add ${remote_name} ${repo_uri}
 #git remote update
