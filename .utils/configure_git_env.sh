af10ac37c549505dbc388fda85df1ac7b7352055
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-06-03 17:33:24 -0500

    forcing images to proper location for doc build

--- .utils/configure_git_env.sh
+++ .utils/configure_git_env.sh
@@ -7,3 +7,4 @@ git rm -r docs/common -r
 git restore -s master templates
 git submodule add https://github.com/aws-quickstart/quickstart-documentation-base-common.git docs/common
 rm configure_git_env.sh
+mv docs/images images
