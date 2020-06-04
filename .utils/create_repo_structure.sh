6ac5a575c1c824155112180a20e436c263ad8f42
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-06-04 09:17:01 -0500

    Adding workflow

--- .utils/create_repo_structure.sh
+++ .utils/create_repo_structure.sh
@@ -9,10 +9,14 @@ mkdir -p docs/generated/regions
 mkdir -p docs/generated/services
 mkdir -p docs/specific
 mkdir -p docs/images
+mkdir -p .github/workflows
 
 # Copying content.
 rsync -avP docs/common/.images/ docs/images/
 rsync -avP docs/common/.specific/ docs/specific/
+
+# enabling workflow.
+cp docs/common/.actions/master-docs-build.yml .github/workflows/
 
 # Create a symlink for easy preview.
 ln -s docs/common/index.adoc docs/index.adoc
