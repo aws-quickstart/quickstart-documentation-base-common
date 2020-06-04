e2df84fedf6408dcd3c49b9f12aab0ecfe96c39f
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-06-04 17:11:30 -0500

    Index to root.

--- .utils/build_docs.sh
+++ .utils/build_docs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-asciidoctor --base-dir docs/ --backend=html5 -o index.html -w --failure-level ERROR --doctype=book -a toc2 docs/common/index.adoc
+asciidoctor --base-dir docs/ --backend=html5 -o ../index.html -w --failure-level ERROR --doctype=book -a toc2 docs/common/index.adoc
