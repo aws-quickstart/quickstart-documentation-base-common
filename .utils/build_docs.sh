ddd64f6645d2a8e0e1801fd6b8e7f417d3e52629
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-06-05 15:09:23 -0500

    Yellow-shading unless deployed in master branch in aws-quickstart owned repo.

--- .utils/build_docs.sh
+++ .utils/build_docs.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-asciidoctor --base-dir docs/ --backend=html5 -o ../index.html -w --failure-level ERROR --doctype=book -a toc2 docs/common/index.adoc
+ASCIIDOC_ATTRIBUTES=""
+GITHUB_REPO_OWNER=$(echo ${GITHUB_REPOSITORY} | cut -d '/' -f 1)
+if [ "${GITHUB_REPO_OWNER}" == "aws-quickstart" ]; then
+  if [ "${GITHUB_REF}" == "refs/heads/master" ]; then
+    ASCIIDOC_ATTRIBUTES="-a production_build"
+  fi
+fi
+asciidoctor --base-dir docs/ --backend=html5 -o ../index.html -w --failure-level ERROR --doctype=book -a toc2 ${ASCIIDOC_ATTRIBUTES} docs/common/index.adoc
