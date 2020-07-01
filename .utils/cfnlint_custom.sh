52ec70d25a56b4bf88b7a31bb964b6ee50d89854
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-07-01 12:24:04 -0500

    removing debugging

--- .utils/cfnlint_custom.sh
+++ .utils/cfnlint_custom.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -e
 if [[ -d "${GITHUB_WORKSPACE}/team_custom_rules" ]]; then
-  CFNLINT_ARGS="-a team_custom_rules"
+  CFNLINT_ARGS="-a ${GITHUB_WORKSPACE}/team_custom_rules/qs_cfn_lint_rules"
   echo "Using custom ruleset"
 else
   echo "NOT using custom ruleset"
 
 fi
 
-cfn-lint --version
-echo;
-cfn-lint --help
-echo;
 cfn-lint ${CFNLINT_ARGS} -i W --templates templates/*
