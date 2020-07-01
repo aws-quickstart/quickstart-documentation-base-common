538a47e93ce6dfc7cf26803e4f50688494a204dc
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-07-01 10:26:27 -0500

    Fixing env

--- .actions/cfnlint-action/entrypoint.sh
+++ .actions/cfnlint-action/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-bash -c "$@"
+sh -c "$@"
--- .utils/cfnlint_custom.sh
+++ .utils/cfnlint_custom.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo "Entered the script"
-set -ex
+set -e
 if [[ -d "${GITHUB_WORKSPACE}/team_custom_rules" ]]; then
   CFNLINT_ARGS="-a team_custom_rules"
 fi
 
 cfn-lint --ignore-checks W ${CFNLINT_ARGS} --templates templates/*
-set +x
