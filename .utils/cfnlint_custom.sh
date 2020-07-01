d8203bacca0a3b2b2b031be905042ce9a5e8fb5e
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-07-01 10:08:16 -0500

    adding debugging

--- .utils/cfnlint_custom.sh
+++ .utils/cfnlint_custom.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-set -e
+echo "Entered the script"
+set -ex
 if [[ -d "${GITHUB_WORKSPACE}/team_custom_rules" ]]; then
   CFNLINT_ARGS="-a team_custom_rules"
 fi
 
 cfn-lint --ignore-checks W ${CFNLINT_ARGS} --templates templates/*
+set +x
