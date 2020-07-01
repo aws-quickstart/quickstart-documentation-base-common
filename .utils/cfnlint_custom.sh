e2c13669bba6bc60109c2ecbc097f6068927581d
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-07-01 12:34:27 -0500

    install module

--- .utils/cfnlint_custom.sh
+++ .utils/cfnlint_custom.sh
@@ -2,6 +2,7 @@
 set -e
 if [[ -d "${GITHUB_WORKSPACE}/team_custom_rules" ]]; then
   pip install -r ${GITHUB_WORKSPACE}/team_custom_rules/requirements.txt
+  python ${GITHUB_WORKSPACE}/team_custom_rules/setup.py install
   CFNLINT_ARGS="-a ${GITHUB_WORKSPACE}/team_custom_rules/qs_cfn_lint_rules"
   echo "Using custom ruleset"
 else
