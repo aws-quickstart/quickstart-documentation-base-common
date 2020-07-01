43d0077de93a0f0d5eca79b8f8b2a92585cb2e6f
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-07-01 11:54:45 -0500

    alpine to buster

--- .actions/cfnlint-action/entrypoint.sh
+++ .actions/cfnlint-action/entrypoint.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-sh -c "$@"
+bash -c "$@"
--- .utils/cfnlint_custom.sh
+++ .utils/cfnlint_custom.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 if [[ -d "${GITHUB_WORKSPACE}/team_custom_rules" ]]; then
   CFNLINT_ARGS="-a team_custom_rules"
+  echo "Using custom ruleset"
+else
+  echo "NOT using custom ruleset"
+
 fi
 
 cfn-lint --ignore-checks W ${CFNLINT_ARGS} --templates templates/*
