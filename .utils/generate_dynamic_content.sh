3e79e15aed05fd386d9a969144c75adf486a998b
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-08-24 12:25:29 -0500

    Pandoc is not a requirement for building docs.

--- .utils/generate_dynamic_content.sh
+++ .utils/generate_dynamic_content.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Wrapper to generate parameter tables within asciidoc workflow.
 set -e
-sudo apt-get install pandoc -y
+#sudo apt-get install pandoc -y
 pip3 install -r docs/boilerplate/.utils/requirements.txt;
 echo "Gen tables"
 python docs/boilerplate/.utils/generate_parameter_tables.py
