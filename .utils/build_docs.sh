249e62913fa44ccbab7847c4b534d8c28a4a8ec2
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-09-01 11:19:01 -0500

    Order of operations fix

--- .utils/build_docs.sh
+++ .utils/build_docs.sh
@@ -2,13 +2,13 @@
 set -e
 ASCIIDOC_ATTRIBUTES=""
 GITHUB_REPO_OWNER=$(echo ${GITHUB_REPOSITORY} | cut -d '/' -f 1)
+if [ -d docs/images ]; then
+  mv docs/images images
+fi
 if [ "${GITHUB_REPO_OWNER}" == "aws-quickstart" ]; then
   cp docs/boilerplate/.css/AWS-Logo.svg images/
   if [ "${GITHUB_REF}" == "refs/heads/master" ]; then
     ASCIIDOC_ATTRIBUTES="-a production_build"
   fi
-fi
-if [ -d docs/images ]; then
-  mv docs/images images
 fi
 asciidoctor --base-dir docs/ --backend=html5 -o ../index.html -w --failure-level ERROR --doctype=book -a toc2 ${ASCIIDOC_ATTRIBUTES} docs/boilerplate/index.adoc
