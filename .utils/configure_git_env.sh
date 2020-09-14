cb2c7918245ecafb33f9cce9301e192222d207eb
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-09-14 12:23:31 -0500

    Forcing usage of main branch.

--- .utils/configure_git_env.sh
+++ .utils/configure_git_env.sh
@@ -1,26 +1,6 @@
-#!/bin/bash -e
-set -x
-git remote update
-git fetch
-set +e 
-git rev-parse --verify origin/gh-pages
-CHECK_BRANCH=$?
-set -e
-if [[  $CHECK_BRANCH -ne 0 ]];then
-  git checkout -b gh-pages
-  git push origin gh-pages
-else
-  git checkout gh-pages
-#    git checkout --track origin/gh-pages
-fi
-git rm -rf .
-touch .gitmodules
-git restore -s origin/master docs
-set +e
-git rm -r docs/boilerplate -r
-rm -rf docs/boilerplate
-set -e
-git restore -s origin/master templates
-git submodule add https://github.com/aws-quickstart/quickstart-documentation-base-common.git docs/boilerplate
-rm configure_git_env.sh
-mv docs/images images
+
+echo "WARNING: Your github actions file is using the MASTER branch of the quickstart-documentation-base-common repo."
+echo "- Edit lines 6 and 16 of your github actions workflow, located in .actions/"
+echo "New projects are not affected"
+echo "NOT BUILDING DOCS"
+exit 1
