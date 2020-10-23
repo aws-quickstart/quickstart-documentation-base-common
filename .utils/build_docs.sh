520d9b0dae956f6e14c75793d2611ca5660d471f
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-10-23 11:02:53 -0500

    Update build_docs.sh

--- .utils/build_docs.sh
+++ .utils/build_docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -e
 ASCIIDOC_ATTRIBUTES=""
 GITHUB_REPO_OWNER=$(echo ${GITHUB_REPOSITORY} | cut -d '/' -f 1)
 if [ -d docs/images ]; then
@@ -12,11 +12,9 @@ fi
 
 #if [ "${GITHUB_REPO_OWNER}" == "aws-quickstart" ]; then
 #  cp docs/boilerplate/.css/AWS-Logo.svg images/
-#  if [ "${GITHUB_REF}" == "refs/heads/master" ] || [ "${GITHUB_REF}" == "refs/heads/main" ] || [ "${DOCBUILD_PROD}" == "true" ]; then
+  if [ "${GITHUB_REF}" == "refs/heads/master" ] || [ "${GITHUB_REF}" == "refs/heads/main" ] || [ "${DOCBUILD_PROD}" == "true" ]; then
     ASCIIDOC_ATTRIBUTES="-a production_build"
-#  fi
+  fi
 #fi
 echo "[Running] => asciidoctor --base-dir docs/ --backend=html5 -o ../index.html -w --failure-level ERROR --doctype=book -a toc2 ${ASCIIDOC_ATTRIBUTES} docs/boilerplate/index.adoc"
 asciidoctor --base-dir docs/ --backend=html5 -o ../index.html -w --failure-level ERROR --doctype=book -a toc2 ${ASCIIDOC_ATTRIBUTES} docs/boilerplate/index.adoc
-
-set +x
