06c2077d49604590918ec48b097f74b51ba66eb2
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-11-09 16:43:48 -0600

    Adding org-level logic back into build script

--- .utils/build_docs.sh
+++ .utils/build_docs.sh
@@ -10,11 +10,11 @@ if [ -f docs/index.html ]; then
   rm docs/index.html
 fi
 
-#if [ "${GITHUB_REPO_OWNER}" == "aws-quickstart" ]; then
-#  cp docs/boilerplate/.css/AWS-Logo.svg images/
+if [ "${GITHUB_REPO_OWNER}" == "aws-quickstart" ]; then
+  cp docs/boilerplate/.css/AWS-Logo.svg images/
   if [ "${GITHUB_REF}" == "refs/heads/master" ] || [ "${GITHUB_REF}" == "refs/heads/main" ] || [ "${DOCBUILD_PROD}" == "true" ]; then
     ASCIIDOC_ATTRIBUTES="-a production_build"
   fi
-#fi
+fi
 echo "[Running] => asciidoctor --base-dir docs/ --backend=html5 -o ../index.html -w --failure-level ERROR --doctype=book -a toc2 ${ASCIIDOC_ATTRIBUTES} docs/boilerplate/index.adoc"
 asciidoctor --base-dir docs/ --backend=html5 -o ../index.html -w --failure-level ERROR --doctype=book -a toc2 ${ASCIIDOC_ATTRIBUTES} docs/boilerplate/index.adoc
