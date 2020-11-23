e3e98072e4d68a5032d967a46642e692cb0b364b
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-11-23 08:45:27 -0600

    Adding example-prod page with preview build

--- .utils/build_docs.sh
+++ .utils/build_docs.sh
@@ -14,7 +14,13 @@ if [ "${GITHUB_REPO_OWNER}" == "aws-quickstart" ]; then
   cp docs/boilerplate/.css/AWS-Logo.svg images/
   if [ "${GITHUB_REF}" == "refs/heads/master" ] || [ "${GITHUB_REF}" == "refs/heads/main" ] || [ "${DOCBUILD_PROD}" == "true" ]; then
     ASCIIDOC_ATTRIBUTES="-a production_build"
-  fi
+  else
+    PREVIEW_BUILD="true"
 fi
 echo "[Running] => asciidoctor --base-dir docs/ --backend=html5 -o ../index.html -w --failure-level ERROR --doctype=book -a toc2 ${ASCIIDOC_ATTRIBUTES} docs/boilerplate/index.adoc"
+
 asciidoctor --base-dir docs/ --backend=html5 -o ../index.html -w --failure-level ERROR --doctype=book -a toc2 ${ASCIIDOC_ATTRIBUTES} docs/boilerplate/index.adoc
+
+if ["${PREVIEW_BUILD}" == "true" ]; then
+  asciidoctor --base-dir docs/ --backend=html5 -o ../prod_example.html -w --failure-level ERROR --doctype=book -a toc2 docs/boilerplate/index.adoc
+fi
